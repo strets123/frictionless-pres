e7e922a43fe8dc186b9ac245edfe76d42ef9170f
strets123 <astretton@zegami.com>
2017-09-23 20:30:18 +0100

    data tweaks

--- smdataproject/tests/test_stream.py
+++ smdataproject/tests/test_stream.py
@@ -6,7 +6,7 @@ from .. import parser
 
 class StreamParserTestCase(unittest.TestCase):
     def test_stream(self):
-        url="https://raw.githubusercontent.com/strets123/frictionless-pres/master/data/smdataset?page[number]=0"
+        url="https://raw.githubusercontent.com/strets123/frictionless-pres/master/data/smdataset%3Fpage%5Bnumber%5D%3D0"
         with tabulator.Stream(
             url,
             format="json-api", 
